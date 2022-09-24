45d8378824e46d8002b3d9740649bc60cb4b9338
191225 <191225yuichirou@gmail.com>
2022-09-24 15:55:20 +0900

    update

--- scripts/index.js
+++ scripts/index.js
@@ -73,9 +73,10 @@ register("pvp", "bot", (test) => {
         if (lastPos.x === SPlayer.location.x.toFixed(1) && lastPos.z === SPlayer.location.z.toFixed(1)) {
             config.debug ? world.say("player stack"):"";
             //for(let i = 0;i<5;i++) 
-            world.say(getBlock(SPlayer.location.x, SPlayer.location.y, SPlayer.location.z));
-            if (getBlock(SPlayer.location.x, SPlayer.location.y, SPlayer.location.z))
-                place(SPlayer);
+            if (getBlock(SPlayer.location.x + 1, SPlayer.location.y, SPlayer.location.z) !== "minecraft:air" &&
+                getBlock(SPlayer.location.x - 1, SPlayer.location.y, SPlayer.location.z) !== "minecraft:air" &&
+                getBlock(SPlayer.location.x, SPlayer.location.y, SPlayer.location.z + 1) !== "minecraft:air" &&
+                getBlock(SPlayer.location.x, SPlayer.location.y, SPlayer.location.z - 1) !== "minecraft:air") place(SPlayer);
 
             /*const r = new BlockRaycastOptions();
             r.maxDistance = 7;
