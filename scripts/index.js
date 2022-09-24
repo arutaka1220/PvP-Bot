45d71c524abd4333062a4f6fe189a3a652ef24c5
arutaka1220 <arutaka.jp@gmail.com>
2022-09-24 12:46:19 +0900

    いろいろかえた

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,65 @@
 import { world, BlockLocation, GameMode, MinecraftEffectTypes, ItemStack, MinecraftItemTypes, Items, EntityRaycastOptions, MinecraftBlockTypes, Player, Location, Vector} from "mojang-minecraft";
 import { register, SimulatedPlayer } from "mojang-gametest";
 import { setTickTimeout as timeout, setTickInterval as interval} from "./scheduling";
-//mieteru?
+
+const config = {
+    name: "PvP Bot",
+    block: MinecraftBlockTypes.planks.createDefaultBlockPermutation(),
+    sword: "minecraft:iron_sword",
+    debug: false
+}
+
 register("pvp", "bot", (test) => {
-    let SPlayer = test.spawnSimulatedPlayer(new BlockLocation(0,0,0), "PvP Bot");
-    let tikai = SPlayer.runCommand('testfor @p[rm=1,name=!"PvP Bot"]').victim[0];
-    /** @type { Player } */
+    // プレイヤー
+    let SPlayer = test.spawnSimulatedPlayer(new BlockLocation(0,0,0), config.name);
+    let tikai = SPlayer.runCommand(`testfor @p[rm=1,name=!"${config.name}"]`).victim[0];
+
+    // 剣を持たせる
+    SPlayer.setItem(new ItemStack(Items.get(config.sword)), 0);
+    SPlayer.runCommand("replaceitem entity @s slot.armor.chest 0 diamond_chestplate");
+    SPlayer.runCommand("replaceitem entity @s slot.armor.feet 0 diamond_boots");
+    SPlayer.runCommand("replaceitem entity @s slot.armor.head 0 diamond_helmet");
+    SPlayer.runCommand("replaceitem entity @s slot.armor.legs 0 diamond_leggings");
+
+    
+    config.debug ? world.say("initialize complete"):"";
+
+    /** ターゲット @type { Player } */
     let player;
     player = getPlayerByName(tikai);
+
+    // 定期的に攻撃先を変える
     interval(() => {
-        tikai = SPlayer.runCommand('testfor @p[rm=1,name=!"PvP Bot"]').victim[0];
+        tikai = SPlayer.runCommand(`testfor @p[rm=1,name=!"${config.name}"]`).victim[0];
         player = getPlayerByName(tikai);
-        let random = Math.floor(Math.random() * 2);
-        if(random === 1) {
-            placeItems(SPlayer, "minecraft:planks", 5)
-        }
+        config.debug ?  world.say(`changed target to ${player.name}`):"";
     }, 10);
-    SPlayer.setItem(new ItemStack(MinecraftItemTypes.ironSword), 0);
+
+    //1tickごとに処理を繰り返す
     interval(() => {
-        SPlayer.dimension;
+        // 攻撃先に移動とかする
         SPlayer.navigateToEntity(player);
         SPlayer.lookAtEntity(player);
+
+        SPlayer.addEffect(MinecraftEffectTypes.speed, 2, 1, false);
+
+        // プレイヤーがターゲットに対して目線を併せていたらターゲットに対して攻撃する
         const r = new EntityRaycastOptions();
         r.maxDistance = 7;
         let a = SPlayer.getEntitiesFromViewVector(r)[0];
         if(a && a.nameTag == tikai) {
             SPlayer.attack();
+            config.debug ?  world.say(`attack to: ${a.name}`):"";
         }
-        player.setRotation(player.rotation.x, -90);
+
+        // y軸に差があったらプレイヤーを登らせたりする
         let sa = Math.trunc(player.location.y) - Math.trunc(SPlayer.location.y);
         if(sa >= 3) {
-            placeItems(SPlayer);
+            config.debug ?  world.say(`block place(y="${sa}")`):"";
+            place(SPlayer);
+        } else if(sa <= -3) {
+            config.debug ?  world.say(`block break(y="${sa}")`):"";
+            break_(SPlayer);
         }
     }, 1);
 
@@ -39,6 +68,7 @@ register("pvp", "bot", (test) => {
 world.events.entityHurt.subscribe(ev => {
     const { hurtEntity, damagingEntity, damage } = ev;
     if(hurtEntity.name == undefined || damagingEntity.name == undefined) return;
+     
     hurtEntity.onScreenDisplay.setActionBar(`§d${damagingEntity.name} §bから §d${damage} §bを受けた`);
     damagingEntity.onScreenDisplay.setActionBar(`§d${hurtEntity.name} §bに §d${damage} §bを与えた`);
 });
@@ -68,12 +98,22 @@ function toBlockLocation(Loc) {
  * 
  * @param {SimulatedPlayer} player 
  */
-function placeItems(player) {
+function place(player) {
     if(getBlock(m(player.location.x),m(player.location.y)-1,m(player.location.z)).id !== "minecraft:air") return;
     player.jump();
     timeout(() => {
         player.setRotation(90,90);
-        const d = MinecraftBlockTypes.planks.createDefaultBlockPermutation();
-        player.dimension.getBlock(new BlockLocation(m(player.location.x),m(player.location.y)-1,m(player.location.z))).setPermutation(d);
+        player.dimension.getBlock(new BlockLocation(m(player.location.x),m(player.location.y)-1,m(player.location.z))).setPermutation(config.block);
     }, 30);
+}
+
+/**
+ * 
+ * @param {SimulatedPlayer} player 
+ */
+function break_(player) {
+    if(getBlock(new Location(m(player.location.x),m(player.location.y)-1,m(player.location.z))).id == "minecraft:air") return;
+    player.setRotation(90,90);
+    const d = MinecraftBlockTypes.air.createDefaultBlockPermutation();
+    getBlock(new Location(m(player.location.x),m(player.location.y)-1,m(player.location.z))).setPermutation(d);
 }
